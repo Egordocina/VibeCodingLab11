bbfb468b9cafd459ccb7ad34f33dfde82d2889a0
Egordocina <ttoitsky.1991@mail.ru>
2026-02-19 15:08:55 +0700

    fix()

--- LAB-1/Program.cs
+++ LAB-1/Program.cs
@@ -37,7 +37,7 @@ namespace ConsoleLoader
 				IEmployee employee;
 				switch (SelectEmployeeType())
 				{
-					//TOOD: отступы+
+					//TOOD: отступы+ ЛЯЛЯЛЯЛЯ
 					case 1:
 					{
 						// Присваивание экземпляра класса HourlyEmployee в переменную
